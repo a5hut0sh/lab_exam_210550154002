c4537497e066273b81de9211372832801a40823a
ashutosh <a7709956406@gmail.com>
2021-08-19 14:01:35 +0530

    sumbit files

---
+++ 1/char_dd.mod
@@ -0,0 +1,2 @@
+/home/pi/device-drivers/lab_exam/1/char_dd.o
+
---
+++ 1/char_dd.mod.c
@@ -0,0 +1,47 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x4766f3ab, "module_layout" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0xadfb6ea0, "cdev_del" },
+	{ 0x31ace3dd, "cdev_add" },
+	{ 0xa8881d43, "cdev_alloc" },
+	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+	{ 0x5f754e5a, "memset" },
+	{ 0x2cfde9a2, "warn_slowpath_fmt" },
+	{ 0xae353d77, "arm_copy_from_user" },
+	{ 0x51a910c0, "arm_copy_to_user" },
+	{ 0x581cde4e, "up" },
+	{ 0xca5a7528, "down_interruptible" },
+	{ 0xc5850110, "printk" },
+	{ 0xb1ad28e0, "__gnu_mcount_nc" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "7B6F2CACBBF1C77EEEE40D5");
